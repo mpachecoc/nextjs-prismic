0434c38bd0580a7738b6fc74626f60e62c10413b
Mauricio Pacheco <mauriciopachecoc@gmail.com>
2020-10-04 17:32:34 -0400

    Initial implementation

---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import Prismic from 'prismic-javascript';
+import { Document } from 'prismic-javascript/types/documents';
+import PrismicDOM from 'prismic-dom';
+
+import SEO from '@/components/SEO';
+import { client } from '@/lib/prismic';
+
+import { Title } from '@/styles/pages/Home';
+
+interface HomeProps {
+  recommendedProducts: Document[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
+
+  return (
+    <div>
+      <SEO title="Your top e-commerce!" shouldExcludeTitleSuffix />
+
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct => (
+            <li key={recommendedProduct.id}>
+              <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                <a>
+                  {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at('document.type', 'product')
+  ]);
+
+  return {
+    props: {
+      recommendedProducts: recommendedProducts.results
+    }
+  }
+}
